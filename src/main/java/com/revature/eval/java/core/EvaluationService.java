2deb07aeaa0c1fe1422a6161e7d616481cf0abdf
carolynrehm <32271621+carolynrehm@users.noreply.github.com>
2018-08-14 16:36:26 -0400

    update EvaluationService

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -14,11 +14,8 @@ public class EvaluationService {
 	 * @return
 	 */
 	public String reverse(String string) {
-		char[] reversed = new char[string.length()];
-		for (int i = reversed.length - 1, j=0; i >= 0; i--, j++) {
-			reversed[j] = string.charAt(i);
-		}
-		return new String(reversed);
+		// TODO Write an implementation for this method declaration
+		return null;
 	}
 
 	/**
